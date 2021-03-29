2490d75813250a800a9c602e2f3455abd67dbd11
SPoon <poonselinasl@gmail.com>
2021-03-29 14:37:07 -0500

    Skill class updated

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,23 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    private String description;
+
+    public Skill () {}
+
+    public Skill (String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return description;
+    }
+
+    public void setLocation(String description) {
+        this.description = description;
+    }
 }
