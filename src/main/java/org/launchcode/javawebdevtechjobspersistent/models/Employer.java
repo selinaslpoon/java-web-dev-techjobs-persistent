7bd0934188ce7a1ba79763db99d6d0fc3f1c7d6d
SPoon <poonselinasl@gmail.com>
2021-03-29 16:06:50 -0500

    Job model updated

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,8 +1,13 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +15,10 @@ public class Employer extends AbstractEntity {
     @NotBlank
     @Size(max=50, message = "Enter shorter location")
     private String location;
+
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer () {}
 
